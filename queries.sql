ff81751bbe0d08e69b7f46443f673c5c169c7223
rafaella-nunes <faellanunes01@gmail.com>
2023-08-14 12:54:06 -0300

    fix: fixes queries

--- queries.sql
+++ queries.sql
@@ -1,9 +1,10 @@
- CREATE TABLE IF NOT EXISTS autores (
+-- Tabelas
+CREATE TABLE IF NOT EXISTS autores (
     id INT PRIMARY KEY,
     nome VARCHAR(255)
 );
 
-  CREATE TABLE IF NOT EXISTS livros (
+CREATE TABLE IF NOT EXISTS livros (
     id INT PRIMARY KEY,
     titulo VARCHAR(255),
     autorID INT,
@@ -13,7 +14,7 @@
     FOREIGN KEY (autorID) REFERENCES autores(id)
 );
 
- CREATE TABLE IF NOT EXISTS users (
+CREATE TABLE IF NOT EXISTS usuarios (
     id INT PRIMARY KEY,
     nome VARCHAR(255),
     email VARCHAR(255),
@@ -21,7 +22,7 @@
     data_cadastro DATE
 );
 
- CREATE TABLE IF NOT EXISTS emprestimos (
+CREATE TABLE IF NOT EXISTS emprestimos (
     id INT PRIMARY KEY,
     livroID INT,
     userID INT,
@@ -30,83 +31,85 @@
     data_devolucao DATE,
     status VARCHAR(50),
     FOREIGN KEY (livroID) REFERENCES livros(id),
-    FOREIGN KEY (userID) REFERENCES users(id)
+    FOREIGN KEY (userID) REFERENCES usuarios(id)
 );
 
- CREATE TABLE IF NOT EXISTS reservas (
+CREATE TABLE IF NOT EXISTS reservas (
     id INT PRIMARY KEY,
     livroID INT,
     userID INT,
     data_reserva DATE,
     data_disponivel DATE,
     FOREIGN KEY (livroID) REFERENCES livros(id),
-    FOREIGN KEY (userID) REFERENCES users(id)
+    FOREIGN KEY (userID) REFERENCES usuarios(id)
 );
 
-
- CREATE TABLE IF NOT EXISTS notificacoes (
+CREATE TABLE IF NOT EXISTS notificacoes (
     notificacaoID SERIAL,
     userID INT,
     mensagem TEXT,
     data_envio DATE,
-    FOREIGN KEY (userID) REFERENCES users(id)
+    FOREIGN KEY (userID) REFERENCES usuarios(id)
 );
 
---Function
-
+-- Função verificar_disponibilidade
 CREATE OR REPLACE FUNCTION verificar_disponibilidade(livroID INT) 
 RETURNS BOOLEAN AS $$
-DECLARE disponivel BOOLEAN;
-DECLARE cnt INTEGER;
-
+DECLARE 
+    disponivel BOOLEAN;
+    cnt INTEGER;
 BEGIN
-    SELECT INTO disponivel
-	COUNT(*)
+    SELECT COUNT(*) INTO cnt
     FROM emprestimos
-    WHERE livroID = livroID AND Status = 'Emprestado';
+    WHERE livroID = livroID AND status = 'Emprestado';
     
-    RETURN NOT disponivel;
+    disponivel := NOT cnt > 0;
+    
+    RETURN disponivel;
 END;
 $$ 
 LANGUAGE plpgsql;
 
+-- Função att_espera
 CREATE OR REPLACE FUNCTION att_espera()
 RETURNS TRIGGER AS $$
-DECLARE livroID INT;
-DECLARE userID INT;
+DECLARE 
+    livro_ID INT;
+    user_ID INT;
 BEGIN
     IF NEW.status = 'Devolvido' THEN
-        SET livroID = NEW.livroID;
+        livro_ID := NEW.livroID;
         
-        SELECT userID INTO userID
+        SELECT userID INTO user_ID
         FROM reservas
-        WHERE livroID = livroID
+        WHERE livroID = livro_ID
         ORDER BY data_reserva
         LIMIT 1;
         
-        IF userID IS NOT NULL THEN
-            -- Enviar notificação para o próximo usuario
-            INSERT INTO notificacoes (userID, mensagem, data_envio) VALUES (userID, 'O livro que você reservou está disponível.', NOW());
+        IF user_ID IS NOT NULL THEN
+            -- Enviar notificação para o próximo usuário
+            INSERT INTO notificacoes (userID, mensagem, data_envio) VALUES (user_ID, 'O livro que você reservou está disponível.', NOW());
         END IF;
     END IF;
+    
+    RETURN NEW;
 END;
 $$
 LANGUAGE plpgsql;
 
-
---Trigger
-
+-- Gatilho att_lista_espera
 CREATE TRIGGER att_lista_espera
 AFTER UPDATE ON emprestimos
+FOR EACH ROW
 EXECUTE FUNCTION att_espera();
 
-
+-- Visão livros_emprestados
 CREATE OR REPLACE VIEW livros_emprestados AS
 SELECT
     E.id,
     L.titulo AS titulo_livro,
     A.nome AS nome_autor,
-    U.nome AS nome_user,
+    U.nome AS nome_usuario,
     E.data_emprestimo,
     E.data_devolucao_programada,
     E.status
@@ -115,5 +118,3 @@ JOIN livros L ON E.livroID = L.id
 JOIN autores A ON L.autorID = A.id
 JOIN usuarios U ON E.userID = U.id
 WHERE E.status = 'Emprestado';
-
-
